6b2ba45d0cfd7c756a015b89a6280bc9a05b61ff
hassansaadfr <saadaoui.hassan@laposte.net>
2021-01-06 11:49:05 +0100

    correction

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hsaadaou <hsaadaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/04 18:38:26 by hsaadaou          #+#    #+#             */
-/*   Updated: 2021/01/06 11:34:31 by hsaadaou         ###   ########.fr       */
+/*   Updated: 2021/01/06 11:47:26 by hsaadaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,7 @@
 
 int		main()
 {
-    ft_printf("+_%10.6s_\n", 0);
-    printf("-_%10.6s_\n", 0);
-    ft_printf("+_%10.10s_\n", 0);
-    printf("-_%10.10s_\n", 0);
-    ft_printf("+_%10.1s_\n", 0);
-    printf("-_%10.1s_\n", 0);
-	ft_printf("+_%p_\n", 0);
-	printf("-_%p_\n", 0);
+    ft_printf("%.3sOp2v%-2um%0.5XBr5OJdC%-XM\n", "H4aTEv6XtNVMsXpPZ7B6XZGQ8WD8U TPJ", 0, -2147483647, 2080778212);
+    printf("%.3sOp2v%-2um%0.5XBr5OJdC%-XM\n", "H4aTEv6XtNVMsXpPZ7B6XZGQ8WD8U TPJ", 0, -2147483647, 2080778212);
 	return (0);
 }
--- srcs/ft_arg_s.c
+++ srcs/ft_arg_s.c
@@ -6,7 +6,7 @@
 /*   By: hsaadaou <hsaadaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/28 13:42:39 by hsaadaou          #+#    #+#             */
-/*   Updated: 2021/01/06 11:21:58 by hsaadaou         ###   ########.fr       */
+/*   Updated: 2021/01/06 11:48:41 by hsaadaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,14 +50,19 @@ static void	ft_treat_s_precision(char *arg, t_prec *lst)
 void		ft_arg_s(va_list arg, t_prec **lst)
 {
 	char	*argument;
+	int		is_empty;
 
+	is_empty = 0;
 	if (*lst)
 	{
 		if ((*lst)->star_precision != NOT_SET)
 			(*lst)->after_dot = (int)va_arg(arg, int);
 		argument = (char*)va_arg(arg, char*);
 		if (!argument)
+		{
 			argument = ft_strdup("(null)");
+			is_empty = 1;
+		}
 		ft_treat_s_precision(argument, *lst);
 		ft_lst_prec_delone(&(*lst));
 	}
@@ -65,8 +70,12 @@ void		ft_arg_s(va_list arg, t_prec **lst)
 	{
 		argument = (char*)va_arg(arg, char*);
 		if (!argument)
+		{
 			argument = ft_strdup("(null)");
+			is_empty = 1;
+		}
 		ft_putstr(argument);
 	}
-	free(argument);
+	if (is_empty)
+		free(argument);
 }
